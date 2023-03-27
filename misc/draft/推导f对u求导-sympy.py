bfa667d5d1dc23d49a8ab724113d3b2f0d01b435
chunleili <li_cl@foxmail.com>
2023-03-27 16:28:53 +0800

    推导f对u求导

---
+++ misc/draft/推导f对u求导-sympy.py
@@ -0,0 +1,29 @@
+#%% 导入模块
+from sympy import *
+from sympy.physics.vector import *
+from sympy.physics.mechanics import *
+from sympy.abc import *
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = 'all'
+
+#%% 初始化
+# 速度
+u = symbols('u', real=True)
+# kinematic map
+G = symbols('G', real=True)
+# 广义坐标
+q = symbols('q', real=True)
+# 时间步长
+dt = symbols('dt', real=True)
+
+q = G * u * dt
+
+U = symbols('U', real=True)
+C = symbols('C', real=True)
+
+U = (1/2) * k *C**2
+
+f = symbols('f', real=True)
+f = G * U.diff(q)
+f
+# %%

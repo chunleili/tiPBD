79590eccae48af59c78b386b058269be2c14c670
chunleili <li_cl@foxmail.com>
2023-03-28 15:01:56 +0800

    refac meta

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 使用方法：
 
-在data/scene中添加场景文件，在models中添加几何文件（现在仅支持tetgen格式），然后从根目录运行`main.py`。
+在data/scene中添加场景文件，在model中添加几何文件（现在仅支持tetgen格式），然后从根目录运行`main.py`。
 
 ```python
 python main.py
--- engine/metadata.py
+++ engine/metadata.py
@@ -20,25 +20,24 @@ class MetaData:
         self.root_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
         self.result_path = os.path.join(self.root_path, "result")
         print("root_path:", self.root_path)
-
-        self.geometry_file = "data/model/bunny1k2k/bunny1k.node"
-        self.dt = 0.001  # timestep size
-        self.inv_h2 = 1.0 / self.dt / self.dt
-        self.lame_lambda = 1e5
-        self.inv_lame_lambda = 1.0/self.lame_lambda
-        self.relax_factor = 0.2
-        self.gravity = ti.Vector([0.0, -9.8, 0.0])
-        self.ground = ti.Vector([0.0, -2.0, 0.0])
-        self.max_iter = 2
-        self.num_substeps = 10
         self.frame = 0
         self.step = 0
-        self.use_multigrid = True
-        self.show_coarse, self.show_fine = True, False
-        self.compute_energy = True
+
+        # self.geometry_file = "data/model/bunny1k2k/bunny1k.node"
+        # self.dt = 0.001  # timestep size
+        # self.inv_h2 = 1.0 / self.dt / self.dt
+        # self.lame_lambda = 1e5
+        # self.inv_lame_lambda = 1.0/self.lame_lambda
+        # self.relax_factor = 0.2
+        # self.gravity = ti.Vector([0.0, -9.8, 0.0])
+        # self.ground = ti.Vector([0.0, -2.0, 0.0])
+        # self.max_iter = 2
+        # self.num_substeps = 10
+        # self.use_multigrid = True
+        # self.show_coarse, self.show_fine = True, False
+        # self.compute_energy = True
 
         # read from json scene file
-        self.use_scene_file = True
         self.use_scene_file = parse_commandline_args().use_scene_file
         if self.use_scene_file:
             self.scene_path = filedialog()

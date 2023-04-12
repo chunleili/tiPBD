7f1bee264519433d5d19fdb5541aabf635c403fc
cl <li_cl@foxmail.com>
2023-04-13 02:57:50 +0800

    use better argparser

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 if __name__ == "__main__":
     import taichi as ti
-    from ui.parse_cli import parse_cli
+    from ui.parse_cli import parse_cli, parse_cli_better
     
-    args = parse_cli()
+    args = parse_cli_better()
 
     if args.use_dearpygui:
         import ui.dearpygui as gui
@@ -10,7 +10,8 @@ if __name__ == "__main__":
         gui_process = Process(target=gui.run)
         gui_process.start()
 
-    ti.init(arch=args.arch, kernel_profiler=args.kernel_profiler, debug=args.debug, device_memory_GB=args.device_memory_GB)
+    # ti.init(arch=args.arch, kernel_profiler=args.kernel_profiler, debug=args.debug, device_memory_GB=args.device_memory_GB)
+    ti.init(**args.init_args)
 
     args.use_solver_main = True
     if args.use_solver_main: # use the provided main in this lib
--- ui/parse_cli.py
+++ ui/parse_cli.py
@@ -3,6 +3,8 @@ def parse_cli():
     Read command line arguments
     '''
     import argparse
+    import taichi as ti
+    import os
     parser = argparse.ArgumentParser(description='taichi PBD')
     parser.add_argument('--scene_file', type=str, default="",
                         help='manually specify scene file, if not specified, use gui to select')
@@ -10,29 +12,71 @@ def parse_cli():
                         help='no gui mode')
     parser.add_argument("--arch", type=str, default="cuda",
                         help="backend(arch) of taichi)")
+    parser.add_argument("--debug", action='store_true', default=False,
+                    help="debug mode")
+    parser.add_argument("--device_memory_fraction", type=float, default=0.5,
+                    help="device memory fraction")
     parser.add_argument("--kernel_profiler", action='store_true', default=False,
                         help="enable kernel profiler")
     parser.add_argument("--use_dearpygui", action='store_true', default=False,
                         help="use dearpygui as gui")
-    parser.add_argument("--debug", action='store_true', default=False,
-                    help="debug mode")
-    parser.add_argument("--device_memory_GB", type=float, default=0.5,
-                    help="device memory in GB")
     args = parser.parse_args()
 
-    import os
-    root_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    # root_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     # args.scene_file =  root_path+"/data/scene/bunny_fluid.json"
-    # args.device_memory_GB = 3.0
 
-    import taichi as ti
     if args.arch == "cuda":
         args.arch = ti.cuda
-    elif args.arch == "gpu":
-        args.arch = ti.gpu
     elif args.arch == "cpu":
         args.arch = ti.cpu
     else:
         args.arch = None
 
+    # 把init_args打包， ti.init(**args.init_args)
+    args.init_args = {"arch": args.arch, "device_memory_fraction": args.device_memory_fraction, "kernel_profiler": args.kernel_profiler, "debug": args.debug}
+    return args
+
+
+def parse_cli_better():
+    import configargparse
+    import taichi as ti
+    import os
+
+    parser = configargparse.ArgumentParser(description='taichi PBD')
+    parser.add_argument('-c', '--config_file', is_config_file=True, help='config file path', default='args.ini')
+    parser.add_argument('--scene_file', type=str, default="",
+                        help='manually specify scene file, if not specified, use gui to select')
+    parser.add_argument('--no-gui', action='store_true', default=False,
+                        help='no gui mode')
+    parser.add_argument("--arch", type=str, default="cuda",
+                        help="backend(arch) of taichi)")
+    parser.add_argument("--debug", action='store_true', default=False,
+                    help="debug mode")
+    parser.add_argument("--device_memory_fraction", type=float, default=0.5,
+                    help="device memory fraction")
+    parser.add_argument("--kernel_profiler", action='store_true', default=False,
+                        help="enable kernel profiler")
+    parser.add_argument("--use_dearpygui", action='store_true', default=False,
+                        help="use dearpygui as gui")
+    args = parser.parse_args()
+    
+    root_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    args.scene_file =  root_path + args.scene_file
+
+    print("----------")
+    print(parser.format_help())
+    print("----------")
+    print(parser.format_values())
+
+    if args.arch == "cuda":
+        args.arch = ti.cuda
+    elif args.arch == "cpu":
+        args.arch = ti.cpu
+    else:
+        args.arch = None
+
+    # 把init_args打包， ti.init(**args.init_args)
+    args.init_args = {"arch": args.arch, "device_memory_fraction": args.device_memory_fraction, "kernel_profiler": args.kernel_profiler, "debug": args.debug}
+
+    print(args)
     return args

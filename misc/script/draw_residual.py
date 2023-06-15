c118eb1afecdeac33dbf159d2748a26465ff79b0
chunleili <li_cl@foxmail.com>
2023-06-15 16:54:43 +0800

    [new] 绘制log y坐标的residual

--- misc/script/draw_residual.py
+++ misc/script/draw_residual.py
@@ -11,9 +11,12 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--onlyfine_iter_range", "-of", nargs=2, type=int, default=(0, 10))
 parser.add_argument("--multigrid_iter_range", "-mg", nargs=2, type=int, default=(0, 10))
+parser.add_argument("--yscale", "-y", type=str, default="log")
 
-onlyfine_start_iter, onlyfine_end_iter = parser.parse_args().onlyfine_iter_range
-multigrid_start_iter, multigrid_end_iter = parser.parse_args().multigrid_iter_range
+args = parser.parse_args()
+
+onlyfine_start_iter, onlyfine_end_iter = args.onlyfine_iter_range
+multigrid_start_iter, multigrid_end_iter = args.multigrid_iter_range
 # %%
 onlyfine = np.loadtxt("result/log/residual_onlyfine.txt")
 onlyfine = onlyfine[onlyfine_start_iter:onlyfine_end_iter]
@@ -31,6 +34,7 @@ if seperate:
     plt.legend()
     plt.figure(figsize=(10, 6))
 plt.plot(mg, label="residual mg", marker="x", markersize=5, color="orange")
+plt.yscale(args.yscale)
 plt.ylabel("residual(2-norm)")
 plt.xlabel("iterations")
 plt.legend()

521ddc9b86b31ddfb79b4ced2b4d630731fe1971
cl <li_cl@foxmail.com>
2023-12-01 16:13:07 +0800

    [fix] fix video.py

--- result/video.py
+++ result/video.py
@@ -3,22 +3,25 @@ import time
 import os
 import shutil
 
-cases = [30,50,100]
+cases = ["cloth3d_256_50_amg",
+         ]
 
-for i in cases:
-    os.chdir(f"result/cloth3d_{i}")
+for case_i in cases:
+    os.chdir(f"result/")
+    os.chdir(case_i)
+
+    mp4_file = case_i+".mp4"
 
     subprocess.call(['powershell', '-Command',"pwd"], shell=True)
 
-    command = ['powershell', '-Command', "img2video", "%04d.png", f"cloth3d_{i}.mp4"]
+    command = ['powershell', '-Command', "img2video", "%04d.png", mp4_file]
     subprocess.call(command, shell=True)
     
     time.sleep(1) 
 
-
     # 使用 shutil 模块的 copy2 函数复制文件
-    source_file = f"cloth3d_{i}.mp4"
-    destination_file = f"../video/cloth3d_{i}.mp4"
+    source_file = mp4_file
+    destination_file = f"../video/"+mp4_file
     shutil.copy2(source_file, destination_file)
     print(f"copy {source_file} to {destination_file} done.")
 

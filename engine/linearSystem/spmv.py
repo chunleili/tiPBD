13d1d46c14831280f1df4e699a7c8528db8d0e52
chunleili <li_cl@foxmail.com>
2024-02-28 22:58:59 +0800

    [new] add 30 N=1000 to 10000 cases

--- engine/linearSystem/spmv.py
+++ engine/linearSystem/spmv.py
@@ -52,16 +52,15 @@ def test_small_case():
     print("diff: ", diff)
 
 
-def test_large_case():
+def test_large_case(N=1000):
     # large case for test
-    print("generating large case")
-    N = 1000
+    # print("generating large case")
     A = sp.random(N, N, density=0.01, format='csr')
     dat = A.data
     ind = A.indices
     ptr = A.indptr
     v = np.random.rand(N).astype(np.float64)
-    print("generated large case")
+    # print("generated large case")
 
     import time
     t0 = time.time()
@@ -73,35 +72,42 @@ def test_large_case():
     # print("first 10 elements of scipy: ", res0[:10])
     # print("first 10 elements of spmv: ", res1[:10])
 
-    print(f"\ntime of scipy:  {t1-t0:.2g}s")
-    print(f"time of my: {t2-t1:.2g}s\n")
+    # print(f"\ntime of scipy:  {t1-t0:.2g}s")
+    # print(f"time of my: {t2-t1:.2g}s\n")
 
-    diff = res0 - res1
-    print("max diff: ", np.max(diff))
-    print("argmax diff: ", np.argmax(diff))
-    print("diff[argmax]: ", diff[np.argmax(diff)])
-    print("res0[argmax]: ", res0[np.argmax(diff)])
-    print("res1[argmax]: ", res1[np.argmax(diff)])
+    # diff = res0 - res1
+    # print("max diff: ", np.max(diff))
+    # print("argmax diff: ", np.argmax(diff))
+    # print("diff[argmax]: ", diff[np.argmax(diff)])
+    # print("res0[argmax]: ", res0[np.argmax(diff)])
+    # print("res1[argmax]: ", res1[np.argmax(diff)])
 
-    print(f"res0.shape: {res0.shape}")
-    print(f"res1.shape: {res1.shape}")
-    print(f"res0.dtype: {res0.dtype}")
-    print(f"res1.dtype: {res1.dtype}")
+    # print(f"res0.shape: {res0.shape}")
+    # print(f"res1.shape: {res1.shape}")
+    # print(f"res0.dtype: {res0.dtype}")
+    # print(f"res1.dtype: {res1.dtype}")
 
-    indptr = A.indptr
+    # indptr = A.indptr
     
-    print(f"last of indptr is {indptr[-1]}")
+    # print(f"last of indptr is {indptr[-1]}")
 
-    print("")
     if np.allclose(res0, res1, atol=1e-6, rtol=1e-3):
         print("result is the same")
+        return True
     else:       
         print("result is different")
         res0 = A@v
         res1 = spmv_csr(dat, ind, ptr, v)
+        return False
     
 
 
 if __name__ == "__main__":
     # test_small_case()
-    test_large_case()
+    for _ in range(30):
+        N = np.random.randint(1000, 10000)
+        print(f"case:{_}\tN: {N}")
+        suc = test_large_case(N)
+        if not suc:
+            exit()
+            break

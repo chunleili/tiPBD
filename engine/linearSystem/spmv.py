18ebbb9bedda1c6d7dc37698a689b374a2fb8558
chunleili <li_cl@foxmail.com>
2024-02-29 13:04:19 +0800

    [refac] refac spmv

--- engine/linearSystem/spmv.py
+++ engine/linearSystem/spmv.py
@@ -1,14 +1,21 @@
 '''
 sparse matrix times vector for taichi
-sparse matrix is given by csr format
+sparse matrix is given by csr(compressed sparse row) format
 
 csr: the column indices for row i are stored in indices[indptr[i]:indptr[i+1]] and their corresponding values are stored in data[indptr[i]:indptr[i+1]].
 
-see https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_array.html#scipy.sparse.csr_array
+See https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_array.html#scipy.sparse.csr_array
 
-A.indptr #first element is 0, last element is nnz   
-A.indices 
-A.data
+
+A.indptr (int   array, size=nrows+1):  ptr (ptr[0]=0, ptr[-1]=nnz) 
+A.indices(int   array, size=nnz)    :  ind
+A.data   (float array, size=nnz)    :  dat
+v        (float array, size=ncols)  :  v
+A = scipy.sparse.csr_array((dat, ind, ptr)) 
+
+res0 = A@v                      # scipy
+res1 = spmv(dat, ind, ptr, v)   # numpy
+res2 = spmv_ti(dat, ind, ptr, v)# taichi
 
 sildes: 【金山文档】 2023-10-25 关于spmv CSR和Matrix-free
 https://kdocs.cn/l/clW7ztNAdwaz
@@ -21,7 +28,7 @@ import time
 
 ti.init(arch=ti.cpu)
 
-def spmv_csr(dat, ind, ptr, v):
+def spmv(dat, ind, ptr, v):
     res = np.zeros((len(ptr)-1), dtype=np.float64)
     dv = np.ones_like(ind, dtype=np.float64) #dat * v    
     v_ = np.zeros_like(ind, dtype=np.float64) #v[ind]
@@ -35,8 +42,9 @@ def spmv_csr(dat, ind, ptr, v):
             res[k] += dv[j]
     return res
 
+
 @ti.kernel
-def spmv_csr_kernel(dat: ti.template(),
+def spmv_kernel(    dat: ti.template(),
                     ind: ti.template(),
                     ptr: ti.template(),
                     v  : ti.template(),
@@ -50,8 +58,8 @@ def spmv_csr_kernel(dat: ti.template(),
             res[k] += dv[j]
 
 
-def spmv_csr_ti(dat, ind, ptr, v):
-    t_start = time.time()
+def spmv_ti(dat, ind, ptr, v):
+    # t_start = time.time()
     res_ti = ti.field(shape=(len(ptr)-1), dtype=ti.f64)
     dv_ti = ti.field(shape=(len(ind)), dtype=ti.f64) 
 
@@ -65,13 +73,13 @@ def spmv_csr_ti(dat, ind, ptr, v):
     ptr_ti.from_numpy(ptr)
     v_ti.from_numpy(v)
 
-    print(f"field: {time.time()-t_start:.2g}s")
-    t_before_kernel = time.time()
-    spmv_csr_kernel(dat_ti, ind_ti, ptr_ti, v_ti, res_ti, dv_ti)
-    t_after_kernel = time.time()
-    print(f"kernel: {t_after_kernel-t_before_kernel:.2g}s")
+    # print(f"field: {time.time()-t_start:.2g}s")
+    # t_before_kernel = time.time()
+    spmv_kernel(dat_ti, ind_ti, ptr_ti, v_ti, res_ti, dv_ti)
+    # t_after_kernel = time.time()
+    # print(f"kernel: {t_after_kernel-t_before_kernel:.2g}s")
     ret = res_ti.to_numpy()
-    print(f"to_numpy: {time.time()-t_after_kernel:.2g}s")
+    # print(f"to_numpy: {time.time()-t_after_kernel:.2g}s")
     return ret
 
 def test_small_case():
@@ -81,7 +89,7 @@ def test_small_case():
     ptr = np.array([0,2,5], dtype=np.int32)
     v = np.array([1,2,3], dtype=np.float32)
 
-    res0 = spmv_csr(dat, ind, ptr, v)
+    res0 = spmv(dat, ind, ptr, v)
     print("res0: ", res0)
 
     A = sp.csr_array((dat, ind, ptr))   
@@ -93,105 +101,46 @@ def test_small_case():
 
 
 def test_large_case(N=1000):
-    # large case for test
-    # print("generating large case")
-    A = sp.random(N, N, density=0.01, format='csr')
-    dat = A.data
-    ind = A.indices
-    ptr = A.indptr
-    v = np.random.rand(N).astype(np.float64)
-    # print("generated large case")
-
-    t0 = time.time()
-    res0 = A@v
-    t1 = time.time()
-    res1 = spmv_csr(dat, ind, ptr, v)
-    t2 = time.time()
-
-    # print("first 10 elements of scipy: ", res0[:10])
-    # print("first 10 elements of spmv: ", res1[:10])
-
-    t_scipy = t1 - t0
-    t_my = t2 - t1
-    print(f"scipy: {t_scipy:.2g}s, my: {t_my:.2g}s")
-    speed_up = t_scipy / t_my
-    if t_my > t_scipy:
-        print(f"my is slower x{speed_up:.2g}")
-    else:
-        print(f"my is faster x{speed_up:.2g}")
-
-    # print(f"\ntime of scipy:  {t1-t0:.2g}s")
-    # print(f"time of my: {t2-t1:.2g}s\n")
-
-    # diff = res0 - res1
-    # print("max diff: ", np.max(diff))
-    # print("argmax diff: ", np.argmax(diff))
-    # print("diff[argmax]: ", diff[np.argmax(diff)])
-    # print("res0[argmax]: ", res0[np.argmax(diff)])
-    # print("res1[argmax]: ", res1[np.argmax(diff)])
-
-    # print(f"res0.shape: {res0.shape}")
-    # print(f"res1.shape: {res1.shape}")
-    # print(f"res0.dtype: {res0.dtype}")
-    # print(f"res1.dtype: {res1.dtype}")
-
-    # indptr = A.indptr
-    
-    # print(f"last of indptr is {indptr[-1]}")
-
-    if np.allclose(res0, res1, atol=1e-6, rtol=1e-3):
-        print("result is the same")
-        return True
-    else:       
-        print("result is different")
-        res0 = A@v
-        res1 = spmv_csr(dat, ind, ptr, v)
-        return False
-    
-
-def test_large_case_with_taichi(N=1000):
     A = sp.random(N, N, density=0.01, format='csr')
     dat = A.data
     ind = A.indices
     ptr = A.indptr
     v = np.random.rand(N).astype(np.float64)
 
-    import time
     t0 = time.time()
     res_scipy = A@v
     t1 = time.time()
-    res_mynp = spmv_csr(dat, ind, ptr, v)
+    res_mynp = spmv(dat, ind, ptr, v)
     t2 = time.time()
-    res_myti = spmv_csr_ti(dat, ind, ptr, v)
+    res_myti = spmv_ti(dat, ind, ptr, v)
     t3 = time.time()
 
     t_scipy = t1 - t0
-    t_mynp = t2 - t1
-    t_myti = t3 - t2
+    t_mynp  = t2 - t1
+    t_myti  = t3 - t2
     print(f"scipy: {t_scipy:.2g}s, mynp: {t_mynp:.2g}s, myti: {t_myti:.2g}s")
-    speed_up = t_scipy / t_myti
 
-    if np.allclose(res_scipy, res_myti, atol=1e-6, rtol=1e-3):
+    if np.allclose(res_scipy, res_myti, atol=1e-6, rtol=1e-3) and\
+       np.allclose(res_scipy, res_mynp, atol=1e-6, rtol=1e-3):
         print("result is the same")
         return True
     else:       
         print("result is different")
         res_scipy = A@v
-        res = spmv_csr_ti(dat, ind, ptr, v)
+        res_mynp = spmv(dat, ind, ptr, v)
+        res_myti = spmv_ti(dat, ind, ptr, v)
         return False
 
 
-def random_test_different_N(func = test_large_case_with_taichi):
+def test_different_N():
     for _ in range(10):
         N = np.random.randint(10000, 20000)
         print(f"\ncase:{_}\tN: {N}")
-        # suc = test_large_case(N)
-        suc = func(N)
+        suc = test_large_case(N)
         if not suc:
             exit()
-            break
+
 
 if __name__ == "__main__":
-    # test_small_case()
-    random_test_different_N()
-    # test_large_case_with_taichi()
+    # test_small_case() # small case for tutorial
+    test_different_N()  # large case for performance test

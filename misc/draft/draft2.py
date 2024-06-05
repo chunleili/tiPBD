068fae1046b746523628793970ccf7deff825d5a
chunleili <li_cl@foxmail.com>
2024-06-05 15:25:29 +0800

    [refac] removing legacy files or gather them

--- misc/draft/draft2.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-np.savez("draft2.npz", 111, 123)
-a = np.load("draft2.npz")
-print(a["arr_0"], a["arr_1"])
--- misc/draft/test_pickle_decode.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-with open("data.pickle", "rb") as f:
-    data = pickle.load(f)
-
-print(data)

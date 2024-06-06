a8dd134632fc5dfea126a303674067985587c09f
chunleili <li_cl@foxmail.com>
2024-06-06 16:53:09 +0800

    refac

--- run.py
+++ run.py
@@ -7,136 +7,130 @@ pythonExe = "python"
 if sys.platform == "darwin":
     pythonExe = "python3"
 
+allargs = [None]
+
 # case1: AMG 1024
-def case1():
-    args = [pythonExe, "engine/cloth/cloth3d.py",
-          "-N=1024",
-          "-solver_type=AMG", 
-          "-delta_t=1e-3", 
-          "-end_frame=800",
-          "-max_iter=100",
-          "-max_iter_Axb=100",
-          "-setup_num=0",
-          "-export_matrix=1",
-          "-export_matrix_interval=50"]
-    log_args(args)
-    call(args)
+args = [pythonExe, "engine/cloth/cloth3d.py",
+        "-N=1024",
+        "-solver_type=AMG", 
+        "-delta_t=1e-3", 
+        "-end_frame=800",
+        "-max_iter=100",
+        "-max_iter_Axb=100",
+        "-setup_num=0",
+        "-export_matrix=1",
+        "-export_matrix_interval=50"]
+allargs.append(args)
+
 
 # case2: GS N1024
-def case2():
-    args = [pythonExe, "engine/cloth/cloth3d.py",
-          "-N=1024",
-          "-solver_type=GS", 
-          "-delta_t=1e-3", 
-          "-end_frame=200",
-          "-max_iter=50",
-          "-max_iter_Axb=150",
-          "-setup_num=0"]
-    log_args(args)
-    call(args)
+args = [pythonExe, "engine/cloth/cloth3d.py",
+        "-N=1024",
+        "-solver_type=GS", 
+        "-delta_t=1e-3", 
+        "-end_frame=200",
+        "-max_iter=50",
+        "-max_iter_Axb=150",
+        "-setup_num=0"]
+allargs.append(args)
 
 
 # case3: XPBD 1024
-def case3():
-    args = [pythonExe, "engine/cloth/cloth3d.py",
-          "-N=1024",
-          "-solver_type=XPBD", 
-          "-delta_t=1e-3", 
-          "-end_frame=200",
-          "-max_iter=100",
-          "-max_iter_Axb=150",
-          "-setup_num=0",
-          ]
-    log_args(args)
-    call(args)
+args = [pythonExe, "engine/cloth/cloth3d.py",
+        "-N=1024",
+        "-solver_type=XPBD", 
+        "-delta_t=1e-3", 
+        "-end_frame=200",
+        "-max_iter=100",
+        "-max_iter_Axb=150",
+        "-setup_num=0",
+        ]
+allargs.append(args)
 
 # case4: XPBD 64
-def case4():
-    args = [pythonExe, "engine/cloth/cloth3d.py",
-          "-N=64",
-          "-solver_type=XPBD", 
-          "-delta_t=1e-3", 
-          "-end_frame=200",
-          "-max_iter=100",
-          "-max_iter_Axb=150",
-          "-setup_num=0",
-          ]
-    log_args(args)
-    call(args)
+args = [pythonExe, "engine/cloth/cloth3d.py",
+        "-N=64",
+        "-solver_type=XPBD", 
+        "-delta_t=1e-3", 
+        "-end_frame=200",
+        "-max_iter=100",
+        "-max_iter_Axb=150",
+        "-setup_num=0",
+        ]
+allargs.append(args)
 
 
 # case5: profile 1024 attach amg
-def case5():
-    args = [pythonExe,"-m","cProfile", 
-            "-o","profile", "engine/cloth/cloth3d.py",
-          "-N=1024",
-          "-solver_type=AMG", 
-          "-delta_t=1e-3", 
-          "-end_frame=5",
-          "-max_iter=100",
-          "-max_iter_Axb=150",
-          "-setup_num=0",
-          "-export_matrix=0",
-          "-out_dir=result/profile1024/",
-          ]
-    log_args(args)
-    call(args)
-    # snakeviz profile
-    # call(["snakeviz", "profile"])
+args = [pythonExe,"-m","cProfile", 
+        "-o","profile", "engine/cloth/cloth3d.py",
+        "-N=1024",
+        "-solver_type=AMG", 
+        "-delta_t=1e-3", 
+        "-end_frame=5",
+        "-max_iter=100",
+        "-max_iter_Axb=150",
+        "-setup_num=0",
+        "-export_matrix=0",
+        "-out_dir=result/profile1024/",
+        ]
+# snakeviz profile
+# call(["snakeviz", "profile"])
+allargs.append(args)
 
 
 # case6: scale 64
-def case6():
-    args = [pythonExe, "engine/cloth/cloth3d.py",
-          "-N=64",
-          "-solver_type=AMG", 
-          "-delta_t=4e-3", 
-          "-end_frame=50",
-          "-max_iter=100",
-          "-max_iter_Axb=150",
-          "-setup_num=1",
-          "-export_matrix=1",
-          "-export_matrix_interval=1",
-          "-out_dir=result/scale64/",
-          ]
-    log_args(args)
-    call(args)
+args = [pythonExe, "engine/cloth/cloth3d.py",
+        "-N=64",
+        "-solver_type=AMG", 
+        "-delta_t=4e-3", 
+        "-end_frame=50",
+        "-max_iter=100",
+        "-max_iter_Axb=150",
+        "-setup_num=1",
+        "-export_matrix=1",
+        "-export_matrix_interval=1",
+        "-out_dir=result/scale64/",
+        ]
+allargs.append(args)
 
 
 # case7: attach 64
-def case7():
-    args = [pythonExe, "engine/cloth/cloth3d.py",
-          "-N=64",
-          "-solver_type=AMG", 
-          "-delta_t=1e-3", 
-          "-end_frame=50",
-          "-max_iter=100",
-          "-max_iter_Axb=150",
-          "-setup_num=0",
-          "-export_matrix=1",
-          "-export_matrix_interval=1",
-          "-out_dir=result/attach64/",
-          ]
-    log_args(args)
-    call(args)
+args = [pythonExe, "engine/cloth/cloth3d.py",
+        "-N=64",
+        "-solver_type=AMG", 
+        "-delta_t=1e-3", 
+        "-end_frame=50",
+        "-max_iter=100",
+        "-max_iter_Axb=150",
+        "-setup_num=0",
+        "-export_matrix=1",
+        "-export_matrix_interval=1",
+        "-out_dir=result/attach64/",
+        ]
+allargs.append(args)
+
 
 # case8: use geo vs. no geo_stiffness
-def case7():
-    args = [pythonExe, "engine/cloth/cloth3d.py",
-          "-N=64",
-          "-solver_type=AMG", 
-          "-delta_t=1e-3", 
-          "-end_frame=51",
-          "-restart=1",
-          "-restart_frame=50",
-          "-restart_dir=result/restart/",
-          "-max_iter=100",
-          "-max_iter_Axb=150",
-          "-setup_num=0",
-          "-export_matrix=1",
-          "-export_matrix_interval=1",
-          "-out_dir=result/geoStiff/",
-          ]
+args = [pythonExe, "engine/cloth/cloth3d.py",
+        "-N=64",
+        "-solver_type=AMG", 
+        "-delta_t=1e-3", 
+        "-end_frame=51",
+        "-restart=1",
+        "-restart_frame=50",
+        "-restart_dir=result/restart/",
+        "-max_iter=100",
+        "-max_iter_Axb=150",
+        "-setup_num=0",
+        "-export_matrix=1",
+        "-export_matrix_interval=1",
+        "-out_dir=result/geoStiff/",
+        ]
+allargs.append(args)
+
+
+def run_case(case_num:int):
+    args = allargs[case_num]
     log_args(args)
     call(args)
 
@@ -147,35 +141,24 @@ def log_args(args:list):
         f.write(f"{args1}\n")
 
 if __name__=='__main__':
-    # if '-case' in sys.argv:
-    #     i = sys.argv.index('-case')
-    #     case_num = int(sys.argv[i+1])
-    # else:
-    #     print('Usage: python run.py -case N, with N=1 or 2, etc.\n')
-    #     sys.exit(1)
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("-case", type=int, default=7,
-                        help="case number, 1 or 2, etc.")
+                        help=f"case number, from 1 to {len(allargs)-1}")
+    parser.add_argument("-list", action="store_true",
+                        help="list all cases")
     case_num = parser.parse_args().case
 
-    # print(f'Running case {case_num}...')
-    if case_num==1:
-        case1()
-    elif case_num==2:
-        case2()
-    elif case_num==3:
-        case3()
-    elif case_num==4:
-        case4()
-    elif case_num==5:
-        case5()
-    elif case_num==6:
-        case6()
-    elif case_num==7:
-        case7()
-    elif case_num==8:
-        case8()
+    if parser.parse_args().list:
+        print("All cases:")
+        for i in range(len(allargs)):
+            print(f"case {i}: {allargs[i]}")
+        sys.exit(0)
+
+    print(f'Running case {case_num}...')
+    
+    if 0 < case_num < len(allargs):
+        run_case(case_num)
     else:
         print('Invalid case number. Exiting...')
         sys.exit(1)

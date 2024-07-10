05166f368079de9160eb942cfc7651b18878b420
cl <li_cl@foxmail.com>
2024-07-10 23:58:56 +0800

    refac rigid_body modes

--- pybind/test_rigid_body_modes.py
+++ pybind/test_rigid_body_modes.py
@@ -7,7 +7,7 @@ print(thisdir/'build/Release')
 sys.path.append(str(thisdir/'build/Release'))  # 添加模块搜索路径
 sys.path.append(str(thisdir/'build/Debug'))  # 添加模块搜索路径
 
-import rigid_body_modes
+import pymgpbd
 import numpy as np
 ndim = 2
 coo = [0.0, 1.0, 2.0, 3.0]  # 示例坐标向量
@@ -16,5 +16,5 @@ B = np.zeros(shape=coo.shape)  # 输出矩阵将填充到这个列表中
 transpose = False  # 可选参数
 
 # 调用函数
-B = rigid_body_modes.rigid_body_modes(ndim, coo, B, transpose)
+B = pymgpbd.rigid_body_modes(ndim, coo, B, transpose)
 print(B)  # 输出结果

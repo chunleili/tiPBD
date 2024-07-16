53f820a3f36a75915b7c541d3582e72359806d95
cl <li_cl@foxmail.com>
2024-07-17 00:59:10 +0800

    add fastA

--- cpp/mgcg_cuda/vcycle.cu
+++ cpp/mgcg_cuda/vcycle.cu
@@ -817,9 +817,38 @@ struct VCycle : Kernels {
     
 };
 
+struct AssembleMatrix : Kernels {
+    CSR<float> A;
+    CSR<float> G;
+    CSR<float> M;
+    Vec<float> alpha_tilde;
+
+    void fetch_A(float *data, int *indices, int *indptr) {
+        CHECK_CUDA(cudaMemcpy(data, A.data.data(), A.data.size() * sizeof(float), cudaMemcpyDeviceToHost));
+        CHECK_CUDA(cudaMemcpy(indices, A.indices.data(), A.indices.size() * sizeof(int), cudaMemcpyDeviceToHost));
+        CHECK_CUDA(cudaMemcpy(indptr, A.indptr.data(), A.indptr.size() * sizeof(int), cudaMemcpyDeviceToHost));
+    }
+
+    void set_G_M(float const *Gp, size_t nG, int const *Gindicesp, size_t nGindices, int const *Gindptrp, size_t nGptr, size_t Grows, size_t Gcols, size_t Gnnz, float const *Mp, size_t nM, int const *Mindicesp, size_t nMindices, int const *Mindptrp, size_t nMptr, size_t Mrows, size_t Mcols, size_t Mnnz) {
+        G.assign(Gp, nG, Gindicesp, nGindices, Gindptrp, nGptr, Grows, Gcols, Gnnz);
+        M.assign(Mp, nM, Mindicesp, nMindices, Mindptrp, nMptr, Mrows, Mcols, Mnnz);
+    }
+
+    void set_alpha_tilde(float *a, size_t n) {
+        alpha_tilde.assign(a, n);
+    }
+
+    void compute_GMG() {
+        CSR<float> GM;
+        spgemm(G, M, GM);
+        spgemm(GM, G, A);
+    }
+};
+
 } // namespace
 
 static VCycle *fastmg = nullptr;
+static AssembleMatrix *fastA = nullptr;
 
 #if _WIN32
 #define DLLEXPORT __declspec(dllexport)
@@ -907,4 +936,20 @@ extern "C" DLLEXPORT void fastmg_RAP(size_t lv) {
 
 extern "C" DLLEXPORT void fastmg_fetch_A(size_t lv, float* data, int* indices, int* indptr) {
     fastmg->fetch_A(lv, data, indices, indptr);
+}
+
+extern "C" DLLEXPORT void fastA_fetch_A(float* data, int* indices, int* indptr) {
+    fastA->fetch_A(data, indices, indptr);
+}
+
+extern "C" DLLEXPORT void fastA_set_G_M(float const *Gp, size_t nG, int const *Gindicesp, size_t nGindices, int const *Gindptrp, size_t nGptr, size_t Grows, size_t Gcols, size_t Gnnz, float const *Mp, size_t nM, int const *Mindicesp, size_t nMindices, int const *Mindptrp, size_t nMptr, size_t Mrows, size_t Mcols, size_t Mnnz) {
+    fastA->set_G_M(Gp, nG, Gindicesp, nGindices, Gindptrp, nGptr, Grows, Gcols, Gnnz, Mp, nM, Mindicesp, nMindices, Mindptrp, nMptr, Mrows, Mcols, Mnnz);
+}
+
+extern "C" DLLEXPORT void fastA_set_alpha_tilde(float *alpha_tilde, size_t n) {
+    fastA->set_alpha_tilde(alpha_tilde, n);
+}
+
+extern "C" DLLEXPORT void fastA_compute_GMG() {
+    fastA->compute_GMG();
 }

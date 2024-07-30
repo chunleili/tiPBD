7d9a6866aec144cd34a82b90e2f661fc66ff31c8
cl <li_cl@foxmail.com>
2024-07-30 22:41:28 +0800

    fix xpbd cloth

--- engine/cloth/cloth3d.py
+++ engine/cloth/cloth3d.py
@@ -592,8 +592,7 @@ def step_xpbd(max_iter):
     for ite in range(max_iter):
         tic_iter = perf_counter()
         reset_dpos(dpos)
-        copy_field(pos, pos_mid)
-        solve_constraints_xpbd(dual_residual, inv_mass, edge, rest_len, lagrangian, dpos, pos_mid)
+        solve_constraints_xpbd(dual_residual, inv_mass, edge, rest_len, lagrangian, dpos, pos)
         update_pos(inv_mass, dpos, pos)
 
         if calc_r:

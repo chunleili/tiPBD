fc6f5545c994b6b3c7fd6ee601bae8c8f53bfd4a
cl <li_cl@foxmail.com>
2024-09-27 15:08:39 +0800

    tee

--- run.py
+++ run.py
@@ -183,7 +183,7 @@ def log_args(args:list):
     #     f.write(f"{args1}\n")
 
 
-# python run.py -end_frame=10 -cases 7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68
+# python run.py -end_frame=10 -cases 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 | Tee-Object -FilePath "output.log"
 if __name__=='__main__':
     Path("result/meta/").mkdir(parents=True, exist_ok=True)
     if os.path.exists(f'result/meta/batch_run.log'):

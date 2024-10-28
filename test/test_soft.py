82b87bacdc3a3cb1efa9380180c660767f2306a7
chunleili <li_cl@foxmail.com>
2024-10-28 15:36:47 +0800

    add test_soft

---
+++ test/test_soft.py
@@ -0,0 +1,21 @@
+import subprocess
+
+def test_soft_amg_python():
+    run_args = ["python", "engine/soft/soft3d.py", "-use_cuda=0", "-solver_type=AMG", "-end_frame=1"]
+    ret = subprocess.check_call(run_args)
+    assert ret == 0
+
+def test_soft_amg_cuda():
+    run_args = ["python", "engine/soft/soft3d.py", "-use_cuda=1", "-solver_type=AMG", "-end_frame=1"]
+    ret = subprocess.check_call(run_args)
+    assert ret == 0
+
+def test_soft_xpbd_gpu():
+    run_args = ["python", "engine/soft/soft3d.py", "-use_cuda=0", "-solver_type=XPBD","-arch=gpu" "-end_frame=1"]
+    ret = subprocess.check_call(run_args)
+    assert ret == 0
+
+def test_soft_xpbd_cpu():
+    run_args = ["python", "engine/soft/soft3d.py", "-use_cuda=0", "-solver_type=XPBD","-arch=gpu", "-end_frame=1"]
+    ret = subprocess.check_call(run_args)
+    assert ret == 0

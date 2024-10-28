fa12a8ed4a1e7eab3ae17960fccfbc7c27ca4f4c
chunleili <li_cl@foxmail.com>
2024-10-28 15:34:56 +0800

    add test_cloth

---
+++ test/test_cloth.py
@@ -0,0 +1,21 @@
+import subprocess
+
+def test_cloth_amg_python():
+    run_args = ["python", "engine/cloth/cloth3d.py","-N=64", "-use_cuda=0", "-solver_type=AMG", "-end_frame=1"]
+    ret = subprocess.check_call(run_args)
+    assert ret == 0
+
+def test_cloth_amg_cuda():
+    run_args = ["python", "engine/cloth/cloth3d.py","-N=64", "-use_cuda=1", "-solver_type=AMG", "-end_frame=1"]
+    ret = subprocess.check_call(run_args)
+    assert ret == 0
+
+def test_cloth_xpbd_gpu():
+    run_args = ["python", "engine/cloth/cloth3d.py","-N=64", "-use_cuda=0", "-solver_type=XPBD","-arch=gpu" "-end_frame=1"]
+    ret = subprocess.check_call(run_args)
+    assert ret == 0
+
+def test_cloth_xpbd_cpu():
+    run_args = ["python", "engine/cloth/cloth3d.py","-N=64", "-use_cuda=0", "-solver_type=XPBD","-arch=gpu", "-end_frame=1"]
+    ret = subprocess.check_call(run_args)
+    assert ret == 0

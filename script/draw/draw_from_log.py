65f13fa5b132840f470ecbacbdd5c94d071db032
chunleili <li_cl@foxmail.com>
2024-11-07 19:51:41 +0800

    refac: merge draw from log into one

--- script/draw/draw_from_log.py
+++ script/draw/draw_from_log.py
@@ -1,16 +1,12 @@
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
-import argparse
 from pathlib import Path
 import os
 
 prj_dir = Path(__file__).parent.parent.parent
 os.chdir(prj_dir)
 
-frame = 99
-r_type = "dual"
-with0 = False
 
 def read_from_log(log_file, frame, r_type, with0=False):
     with open(log_file, "r") as f:
@@ -25,7 +21,7 @@ def read_from_log(log_file, frame, r_type, with0=False):
         lines = [line for line in lines if line.startswith(f"{frame}-")]
         # print(lines)
 
-        # 提取出dual_r后面的数字， dual_r在行中间
+        # 提取出r_type后面的数字， r_type在行中间
         r = [line for line in lines if f"{r_type}:" in line]
         r0 = []
         for i,l in enumerate(r):
@@ -34,34 +30,26 @@ def read_from_log(log_file, frame, r_type, with0=False):
                 r0.append( float(l.split(f"{r_type}0:")[1].split()[0]))
     return r, r0
 
+
+def run_and_draw(log_file, ax, legend):
+    r, r0= read_from_log(log_file, frame, r_type, with0)
+    r = np.array(r)
+    print(r[:5])
+    ax.plot(r)
+    ax.set_ylabel(f"{r_type}")
+    ax.set_yscale("log")
+    ax.legend([legend])
+
+frame = 74
+r_type = "dual"
+with0 = False
+fig,axs = plt.subplots(4)
 log_file = "result/latest/latest.log"
-r, r0 = read_from_log(log_file, frame, r_type, with0)
-
-log_file = "result/case148-1106-XPBD-strain/latest.log"
-r2, r02 = read_from_log(log_file, frame, r_type, with0)
-
-print(r2)
-if with0:
-    print("initial:",r02[0])
-    r.insert(0, r02[0])
-
-fig,axs = plt.subplots(2)
-axs[0].plot(r[:])
-axs[1].plot(r2[:])
-
-plt.xlabel("iteration")
-axs[0].set_ylabel(f"dual residual")
-axs[1].set_ylabel(f"dual residual")
-
-axs[0].legend([f"AMG"])
-axs[1].legend([f"XPBD"])
-# plt.title(f"{case_name} frame {frame}")
-# 设定y轴采用科学计数法
-# axs[0].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-# axs[1].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-# 设定y轴采用log
-axs[0].set_yscale("log")
-axs[1].set_yscale("log")
-# 设定x轴只能用整数
-plt.gca().xaxis.get_major_locator().set_params(integer=True)
+run_and_draw(log_file, axs[0], r_type)
+r_type = "Newton"
+run_and_draw(log_file, axs[1], r_type)
+r_type = "energy"
+run_and_draw(log_file, axs[2], r_type)
+r_type = "strain"
+run_and_draw(log_file, axs[3], r_type)
 plt.show()

cd80bfddaf3d1009eb8e02b2c78e45a1ebe62512
chunleili <li_cl@foxmail.com>
2025-01-17 15:35:07 +0800

    fix run.py ball cases name

--- run.py
+++ run.py
@@ -2023,8 +2023,12 @@ allargs.append(args)
 
 # case184-186: ball for different size
 for i,config in enumerate(["ball1k/ball1k.node","ball22k/ball22k.node","ball99k/ball99k.node"]):
+    solver = "AMG"
+    dt = 10e-3
+    model = config.split("/")[0]
+    casenames[len(allargs)] = f"{model}-{solver}"
     args = ["engine/soft/soft3d.py",
-            f"-out_dir=result/case{len(allargs)}-{day}-bunny",
+            f"-out_dir=result/case{len(allargs)}-{day}-{casenames[len(allargs)]}",
             f"-auto_another_outdir={auto_another_outdir}",
             f"-model_path=data/model/{config}",
             "-tol=1e-3",
@@ -2043,8 +2047,12 @@ for i,config in enumerate(["ball1k/ball1k.node","ball22k/ball22k.node","ball99k/
 
 # case187-189: ball for different size
 for i,config in enumerate(["ball1k/ball1k.node","ball22k/ball22k.node","ball99k/ball99k.node"]):
+    solver = "XPBD"
+    dt = 10e-3
+    model = config.split("/")[0]
+    casenames[len(allargs)] = f"{model}-{solver}"
     args = ["engine/soft/soft3d.py",
-            f"-out_dir=result/case{len(allargs)}-{day}-bunny",
+            f"-out_dir=result/case{len(allargs)}-{day}-{casenames[len(allargs)]}",
             f"-auto_another_outdir={auto_another_outdir}",
             f"-model_path=data/model/{config}",
             "-tol=1e-3",
@@ -2055,11 +2063,37 @@ for i,config in enumerate(["ball1k/ball1k.node","ball22k/ball22k.node","ball99k/
             "-end_frame=300",
             "-mu=1e9",
             "-reinit=freefall",
-            f"-time_budget={1}"
+            f"-time_budget={0.1}"
             ]
     allargs.append(args)
 
 
+
+
+#case 190-195: beam for different mu=1e5 1e7 1e9, timebudget=0.1, dt=10ms 
+for i, config in enumerate(["1e5","1e7","1e9"]*2):
+    if i<=2:
+        solver = "AMG"
+    else:
+        solver = "XPBD"
+    model = "beam9k"
+    casenames[len(allargs)] = f"{model}-{config}-{solver}"
+    args = ["engine/soft/soft3d.py",
+        f"-out_dir=result/case{len(allargs)}-{day}-{casenames[len(allargs)]}",
+        f"-model_path=data/model/{model}/{model}.geo",
+        "-tol=1e-3",
+        "-delta_t=10e-3",
+        f"-solver_type={solver}",
+        "-arch=gpu",
+        "-maxiter=10000",
+        "-end_frame=100",
+        "-pmass=1",
+        f"-mu={config}",
+        "-use_gravity=1",
+        "-reinit=beam",
+        "-time_budget=0.1"
+        ]
+    allargs.append(args)
 
 
 def export_cases_to_json(allargs):

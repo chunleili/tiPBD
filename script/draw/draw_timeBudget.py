6be0a41db713ff2275fb22aa12604504b796db8c
chunleili <li_cl@foxmail.com>
2025-01-18 18:17:17 +0800

    draw timebudget

---
+++ script/draw/draw_timeBudget.py
@@ -0,0 +1,81 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+import os
+
+prj_dir = Path(__file__).parent.parent.parent
+os.chdir(prj_dir)
+
+
+def read_from_log(log_file, frame, r_type, with0=False):
+    with open(log_file, "r") as f:
+        # 读取所有字符串
+        lines = f.readlines()
+        #去掉 leading and trailing whitespaces
+        lines = [line.strip() for line in lines]
+        # 去掉空行
+        lines = [line for line in lines if line]
+
+        # 提取出特定的frame, 例如以20-开头
+        lines = [line for line in lines if line.startswith(f"{frame}-")]
+        # print(lines)
+
+        # 提取出r_type后面的数字， r_type在行中间
+        r = [line for line in lines if f"{r_type}:" in line]
+        r0 = []
+        for i,l in enumerate(r):
+            r[i] = float(l.split(f"{r_type}:")[1].split()[0])
+            if with0:
+                r0.append( float(l.split(f"{r_type}0:")[1].split()[0]))
+
+        # 提取出FramePastTime后面的数字
+        FramePastTime = [line for line in lines if f"FramePastTime:" in line]
+        for i,l in enumerate(FramePastTime):
+            FramePastTime[i] = float(l.split(f"FramePastTime:")[1].split()[0])
+    return r, r0, FramePastTime
+
+def draw(x,y,ax):
+    ax.plot(x, y)
+    ax.set_xlabel(f"Wall Time(ms)", fontsize=15)
+    ax.set_ylabel(f"Dual Residual", fontsize=15)
+    ax.set_yscale("log")
+    ax.tick_params(axis='both', which='major', labelsize=15)  # 加大 tick 的字体
+    return ax
+
+frame = 2
+r_type = "dual"
+with0 = True
+fig,axs = plt.subplots(1,squeeze=True)
+log_file = "result/case166-0116-bunny/latest.log"
+r, r0, FramePastTime= read_from_log(log_file, frame, r_type, with0)
+r = np.array(r)
+print(r[:5])
+r0__ = r[0]
+print(r.shape)
+
+# 加粗线条
+axs.plot(FramePastTime, r, linewidth=2)
+axs.set_xlabel(f"Wall Time(ms)", fontsize=15)
+axs.set_ylabel(f"Dual Residual", fontsize=15)
+axs.set_yscale("log")
+axs.tick_params(axis='both', which='major', labelsize=15)  # 加大 tick 的字体
+
+
+log_file = "result/case162-0116-bunny/latest.log"
+r, r0, FramePastTime= read_from_log(log_file, frame, r_type, with0)
+# run_and_draw(log_file, axs, "MGPBD")
+r.insert(0, r0__)
+FramePastTime.insert(0, 0)
+r = np.array(r)
+axs.plot(FramePastTime, r)
+# axs.legend(["XPBD", "MGPBD"],font)
+axs.legend(["XPBD", "MGPBD"], fontsize=15)
+print(r.shape)
+# r_type = "Newton"
+# run_and_draw(log_file, axs[1], r_type)
+# r_type = "energy"
+# run_and_draw(log_file, axs[2], r_type)
+# r_type = "strain"
+# run_and_draw(log_file, axs[3], r_type)
+plt.show()

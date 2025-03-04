f9eda68131785a8e44bb34a1cb62adaba2bcbc5e
cl <li_cl@foxmail.com>
2025-03-04 18:39:29 +0800

    add json parser and yaml to json converter

--- engine/common_args.py
+++ engine/common_args.py
@@ -36,7 +36,8 @@ def add_common_args(parser):
     parser.add_argument("-amgx_config", type=str, default="data/config/FGMRES_CLASSICAL_AGGRESSIVE_PMIS.json")
     parser.add_argument("-export_state", type=int, default=False)
     parser.add_argument("-use_json", type=int, default=False, help="json configs will overwrite the command line args")
-    parser.add_argument("-json_path", type=str, default="data/scene/cloth/config.json", help="json configs will overwrite the command line args")
+    parser.add_argument("-json_path", type=str, default="", help="json configs will overwrite the command line args")
+    parser.add_argument("-yaml_path", type=str, default="", help="yaml configs")
     parser.add_argument("-gravity", type=float, nargs=3, default=(0.0, 0.0, 0.0))
     parser.add_argument("-use_gravity", type=int, default=True)
     parser.add_argument("-converge_condition", type=str, default="dual", choices=["dual", "Newton", "strain"], help="dual: dual residual, Newton: sqrt(dual^2+primal^2), strain: strain limiting")
@@ -51,16 +52,40 @@ def add_common_args(parser):
     parser.add_argument("-use_ground_collision", type=int, default=False)
     parser.add_argument("-geo_dir", type=str, default=f"data/model/extraSpring/")
     parser.add_argument("-use_extra_spring", type=int, default=False)
+    parser.add_argument("-use_external_constraints", type=int, default=False)
     parser.add_argument("-use_pintotarget", type=int, default=False)
     parser.add_argument("-use_muscle2muscle", type=int, default=False)
     parser.add_argument("-start_frame", type=int, default=1)
     parser.add_argument("-clean_dir", type=int, default=False)
     parser.add_argument("-export_fulldual", type=int, default=False)
     parser.add_argument("-time_budget", type=float, default=1000.0)
+    parser.add_argument("-use_time_budget", type=int, default=False)
     parser.add_argument("-calc_rbm", type=int, default=False)
     parser.add_argument("-local_interval", type=int, default=0)
     parser.add_argument("-pmass", type=float, default=1.0)
     parser.add_argument("-use_totalmass", type=int, default=0)
     parser.add_argument("-total_mass", type=float, default=-1)#16000.0
+    parser.add_argument("-mass_density", type=float, default=-1)#1000.0
     parser.add_argument("-direct_solver_type", type=str, default="cusolver", choices= ["cusolver", "scipy", "pardiso"])
+    parser.add_argument("-fixed_stiffness", type=float, default=1e8)
+    parser.add_argument("-fixed_particles", type=int, nargs="*", default=[])
     return parser
+
+
+def parse_json_args(args,json_path):
+    import json
+    import os
+    if not os.path.exists(json_path):
+        assert False, f"json file {json_path} not exist!"
+    print(f"CAUTION: using json config file {json_path} to overwrite the command line args!")
+    with open(json_path, "r") as json_file:
+        config = json.load(json_file)
+    for key, value in config.items():
+        if hasattr(args,key):
+            if getattr(args,key) != value:
+                print(f"overwriting {key} from {getattr(args,key)} to {value}")
+                setattr(args,key,value)
+        else:
+            print(f"Add new json key {key}:{value} to args")
+            setattr(args,key,value)
+    return args

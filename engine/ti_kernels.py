ad4ea2becea7eafed56d021bfd24063021c5a686
cl <li_cl@foxmail.com>
2025-03-06 11:26:47 +0800

    fix calc_dual_kernel by add loop config

--- engine/ti_kernels.py
+++ engine/ti_kernels.py
@@ -38,6 +38,7 @@ def calc_dual_kernel(alpha_tilde:ti.template(),
                        constraints:ti.template(),
                        dual_residual:ti.template())->ti.f32:
     dual = 0.0
+    ti.loop_config(serialize=True)
     for i in range(dual_residual.shape[0]):
         dual_residual[i] = -(constraints[i] + alpha_tilde[i] * lagrangian[i])
         dual += dual_residual[i] * dual_residual[i]

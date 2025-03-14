1504c4c82fb962d0c6b583709dd46f52b1e11e24
cl <li_cl@foxmail.com>
2025-03-14 15:49:45 +0800

    Revert "add energy.cu from ruan's code"
    This reverts commit 175a3cad1a31b24e956e68bb193a9dca5c39172d.

--- cpp/mgcg_cuda/cuda_utils.cuh
+++ cpp/mgcg_cuda/cuda_utils.cuh
@@ -1,8 +1,7 @@
 #pragma once
 #include <cuda_runtime.h>
 #include <chrono>
-#include <thrust/random.h>  
-#include <thrust/device_vector.h>  
+
 /* -------------------------------------------------------------------------- */
 /*                                 erro check                                 */
 /* -------------------------------------------------------------------------- */
